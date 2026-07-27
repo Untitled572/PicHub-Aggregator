ef1e567748c1818bff526ec57e662be27ef6d372
Untitled572 <rongchangjin@126.com>
2026-07-27 23:09:44 +0800

    fix: switch to modernc.org/sqlite (pure Go), drop CGo dependency

--- backend/store/sqlite.go
+++ backend/store/sqlite.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"time"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 
 	"github.com/pichub/backend/model"
 )
@@ -16,7 +16,7 @@ type Store struct {
 }
 
 func New(dbPath string) (*Store, error) {
-	db, err := sql.Open("sqlite3", dbPath+"?_journal_mode=WAL&_foreign_keys=on")
+	db, err := sql.Open("sqlite", dbPath+"?_timeout=5000&_journal_mode=WAL&_foreign_keys=on")
 	if err != nil {
 		return nil, fmt.Errorf("open db: %w", err)
 	}
